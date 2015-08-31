0293bfae318d44e498aad3266fa332023d514e02
mikebutts <mgbutts@gmail.com>
2015-08-31 15:01:58 -0400

    random start points

--- asteroid.js
+++ asteroid.js
@@ -5,12 +5,18 @@ var view = {
   height: $("#drawCanvas").height(),
 
   createAsteroid: function(){
-    var radius = Math.floor((Math.random() * 30) + 20);
-    var startX = Math.floor((Math.random() * 30) + 20); 
-    var startY = Math.floor((Math.random() * 30) + 20);
+    var radius = Math.floor(Math.random() * 30 + 20);
+    var startX = Math.floor(Math.random() * view.height); 
+    (startX >= view.height/ 2) ? startX += view.height : startX -= view.height;
+    var startY = Math.floor(Math.random() * view.width);
+    (startY >= view.width/ 2) ? startY += view.width : startY -= view.width;
+
+
+      console.log(startX);
+      console.log(startY);
 
     view.ctx.beginPath();
-    view.ctx.arc(100,75,radius,0,2*Math.PI);
+    view.ctx.arc(startX , startY ,radius,0,2*Math.PI);
     view.ctx.stroke();
   }
 };

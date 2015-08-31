784fc0ce392e8fd9bd60db031a2123ebfbfbf48d
mikebutts <mgbutts@gmail.com>
2015-08-31 18:31:34 -0400

    rendering

--- asteroid.js
+++ asteroid.js
@@ -6,9 +6,10 @@ var view = {
   bgReady: false,
   bgImage: new Image(),
 
-  init: function(){
+  init: function(asteroids){
     window.onload = view.runbgImage();
-    view.render();
+    view.renderAsteroids(asteroids);
+    view.renderBackground();
   },
 
   runbgImage: function () {
@@ -24,24 +25,29 @@ var view = {
 
   renderAsteroids: function(asteroids) {
     for(var i = 0; i < model.numOfAsteroids; i++){
-      model.ctx.beginPath();
-      model.ctx.arc(asteroid[0], asteroid[1], asteroid[2], 0, 2*Math.PI);
-      model.ctx.stroke();
+      view.ctx.beginPath();
+      view.ctx.arc(asteroids[i][0], asteroids[i][1], asteroids[i][2], 0, 2*Math.PI);
+      console.log(asteroids[i][0],asteroids[i][1],asteroids[i][2] );
+      view.ctx.stroke();
     }
   },
 
   moveAsteroid: function(asteroid){
-    var velocityX = Math.floor(Math.random() * 5);
-    var velocityY = Math.floor(Math.random() * 5);
-    this.move = function(asteroid){
-    asteroid.coordinateX += velocityX;
-    asteroid.coordinateY += velocityY;
+    var velocityX = Math.floor(Math.random() * 30);
+    var velocityY = Math.floor(Math.random() * 30);
+  
+        asteroid[0] += velocityX;
+        asteroid[1] += velocityY;
     // console.log(this.coordinateY, this.coordinateX);
-    };
+    
   },
 
-  gameLoop: function(){
+  gameLoop: function(asteroids){
+    for (var i = asteroids.length - 1; i >= 0; i--) {
+           view.moveAsteroid(asteroids[i]);
+      };
 
+    view.renderAsteroids(asteroids);      
   }
 
 };
@@ -57,11 +63,11 @@ var model = {
   },
 
   createAsteroid: function(){
-    position = [];
+    var position = [];
     var radius = Math.floor(Math.random() * 30 + 20);
     var startX = Math.floor(Math.random() * view.height);
     var startY = Math.floor(Math.random() * view.width);
-    position.push(startX).push(startY).push(radius);
+    position.push(startX,startY,radius);
     return position;
   },
 
@@ -82,12 +88,15 @@ var controller = {
 
   init: function(){
     model.init();
-    view.init(asteroidState);
+    view.init(model.asteroidState);
     controller.gameLoop();
   },
 
   gameLoop: function(){
-    window.gameLoop = window.setInterval(view.gameLoop, 500);
+    window.gameLoop = window.setInterval(function(){
+        view.gameLoop(model.asteroidState);
+    }, 50);
+
   },
 
   endGame: function(){

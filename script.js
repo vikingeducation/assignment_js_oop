ad433599d2593990aae6c2bf67af6edac3cf0a4f
David <davidmeza@outlook.com>
2015-08-31 17:51:33 -0400

    add collision check

--- script.js
+++ script.js
@@ -9,14 +9,20 @@ var asteroidsModel = {
 
   generateAsteroid: function () {
 
-    vel = sample(10)
-    var v1 = Math.floor(Math.random() * vel + (vel / 10))
-    var v2 = Math.floor(Math.random() * vel + (vel / 10))
+    vel1 = this.sample(10)
+    var v1 = Math.floor(Math.random() * vel1 + (vel1 / 10))
+    vel2 = this.sample(10)
+    var v2 = Math.floor(Math.random() * vel2 + (vel2 / 10))
 
     var c1 = Math.floor(Math.random() * view.canvasWidth)
     var c2 = Math.floor(Math.random() * view.canvasHeight)
-    var r = Math.floor(Math.random() * 100 + 50)
+    var r = Math.floor(Math.random() * 50 + 50)
     this.asteroids.push(new Asteroid([v1, v2], [c1, c2], r))
+  },
+
+  sample: function(n){
+    vals = [n, -n]
+    return vals[Math.floor(Math.random() * 2)]
   },
 
   spawnAsteroids: function(){
@@ -26,10 +32,35 @@ var asteroidsModel = {
   },
 
   updateAsteroids: function(){
+    this.checkCollision();
     for (var i = 0; i < this.asteroids.length; i++){
       this.asteroids[i].coordinates[0] += this.asteroids[i].velocity[0];
       this.asteroids[i].coordinates[1] += this.asteroids[i].velocity[1];
     }
+  },
+
+  checkCollision: function() {
+    var asteroids = this.asteroids
+    var collisions = [];
+    for (var i = 0; i < asteroids.length; i++) {
+      for (var j = i + 1; j < asteroids.length; j++) {
+        var radiusDiff = Math.pow((asteroids[i].radius - asteroids[j].radius), 2)
+        var radiusSum = Math.pow((asteroids[i].radius + asteroids[j].radius), 2)
+        var xCoordsDiff = Math.pow((asteroids[i].coordinates[0] - asteroids[j].coordinates[0]), 2)
+        var yCoordsDiff = Math.pow((asteroids[i].coordinates[1] - asteroids[j].coordinates[1]), 2)
+        // (R0-R1)^2 <= (x0-x1)^2+(y0-y1)^2 <= (R0+R1)^2
+        if ( radiusDiff <= (xCoordsDiff + yCoordsDiff) && (xCoordsDiff + yCoordsDiff) <= radiusSum ) {
+          // console.log("asteroid " + asteroids[i] + " and asteroid " asteroids[j] + "collided");
+          collisions.push(i);
+          collisions.push(j);
+        }
+      };
+    };
+    console.log(collisions.length + "collisions occurred")
+    for (var i = collisions.length - 1; i >= 0 ; i--) {
+      var collidedAsteroid = collisions[i].splice(i, i + 1)
+    };
+
   }
 
 }
@@ -38,22 +69,29 @@ var view = {
 
   canvasHeight: 500,
   canvasWidth: 700,
+  canvas: null,
+  context: null,
 
   init: function () {
-    // this.placeAsteroids();
+    this.setVars();
     this.render();
   },
 
+  setVars: function() {
+    this.canvas = document.getElementById('asteroid-canvas');
+    this.context = this.canvas.getContext("2d");
+  },
+
   render: function(){
-          setInterval(function(){
-            asteroidsModel.updateAsteroids();
-            view.draw();
-          }, 1000)
-      },
+    setInterval(function(){
+      asteroidsModel.updateAsteroids();
+      view.draw();
+    }, 500);
+  },
 
   draw: function(){
-    var canvas = document.getElementById('asteroid-canvas');
-    var context = canvas.getContext("2d");
+    var canvas = this.canvas;
+    var context = this.context
     context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     for (var i = 0; i < asteroidsModel.asteroids.length; i++){
       context.beginPath();
@@ -79,16 +117,11 @@ $(document).ready(function() {
   controller.init();
 })
 
-
 function Asteroid(velocity, coordinates, radius){
 
   this.velocity = velocity
   this.coordinates = coordinates
   this.radius = radius
-  // this.tic = function(){
-  //   this.coordinates[0] += this.velocity[0]
-  //   this.coordinates[1] += this.velocity[1]
-  // }
 
 }
 
@@ -99,30 +132,16 @@ Asteroid.prototype.tic = function(){
 
 }
 
-var asteroidSim = function(){
-  asteroids = []
-  for (var i = 0; i < 10; i++){
-    v1 = Math.floor(Math.random() * 100)
-    v2 = Math.floor(Math.random() * 100)
-    c1 = Math.floor(Math.random() * 100)
-    c2 = Math.floor(Math.random() * 100)
-    asteroids.push(new Asteroid([v1, v2], [c1, c2]))
-  }
+// Two asteroids collide
+// For each of them, we divide the radius by 2
+// If radius is below 5 we remove it from screen
+// Otherwise we generate two more asteroids with the close coordinates but opposite directions
 
-  start = Date.now()
 
-  for (var i = 0; i < asteroids.length; i++){
-    for (var j = 0; j < 10000; j++){
-      asteroids[i].tic()
-    }
-  }
+Asteroid.prototype.tic = function(){
 
-  console.log(Date.now() - start)
+  this.coordinates[0] += this.velocity[0]
+  this.coordinates[1] += this.velocity[1]
 
-}
-
-var sample = function(n){
-  vals = [n, -n]
-  return vals[Math.round(Math.random())]
 }
 

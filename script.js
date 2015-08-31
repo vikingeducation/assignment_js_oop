07c4da719c62e25a692bf37a16911117d1a420d7
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-08-31 17:05:30 -0400

    created canvas

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ var asteroidsModel = {
   asteroids: [],
 
   init: function () {
-    this.generateAsteroid();
+    this.spawnAsteroids();
+    // this.generateAsteroid();
   },
 
   generateAsteroid: function () {
-    v1 = Math.floor(Math.random() * 10)
-    v2 = Math.floor(Math.random() * 10)
-    c1 = Math.floor(Math.random() * view.canvasWidth)
-    c2 = Math.floor(Math.random() * view.canvasHeight)
-    this.asteroids.push(new Asteroid([v1, v2], [c1, c2]))
+
+    vel = sample(10)
+    var v1 = Math.floor(Math.random() * vel + (vel / 10))
+    var v2 = Math.floor(Math.random() * vel + (vel / 10))
+
+    var c1 = Math.floor(Math.random() * view.canvasWidth)
+    var c2 = Math.floor(Math.random() * view.canvasHeight)
+    var r = Math.floor(Math.random() * 100 + 50)
+    this.asteroids.push(new Asteroid([v1, v2], [c1, c2], r))
+  },
+
+  spawnAsteroids: function(){
+    for (var i = 0; i < Math.floor(Math.random() * 10); i++){
+      this.generateAsteroid();
+    }
+  },
+
+  updateAsteroids: function(){
+    for (var i = 0; i < this.asteroids.length; i++){
+      this.asteroids[i].coordinates[0] += this.asteroids[i].velocity[0];
+      this.asteroids[i].coordinates[1] += this.asteroids[i].velocity[1];
+    }
   }
 
 }
@@ -22,12 +40,29 @@ var view = {
   canvasWidth: 700,
 
   init: function () {
-    this.placeAsteroids();
+    // this.placeAsteroids();
+    this.render();
   },
 
-  placeAsteroids: function () {
-    // var canvasContext = document.getElementById('asteroid-canvas').getContext("2d");
-    // document.getElementById('asteroid-canvas').fillRect(50, 25, 150, 100);
+  render: function(){
+          setInterval(function(){
+            asteroidsModel.updateAsteroids();
+            view.draw();
+          }, 1000)
+      },
+
+  draw: function(){
+    var canvas = document.getElementById('asteroid-canvas');
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    for (var i = 0; i < asteroidsModel.asteroids.length; i++){
+      context.beginPath();
+      context.arc(asteroidsModel.asteroids[i].coordinates[0], asteroidsModel.asteroids[i].coordinates[1],
+                       (asteroidsModel.asteroids[i].radius), 0, Math.PI * 2, false);
+      context.strokeStyle = "#000";
+      context.stroke();
+    }
+
   }
 
 }
@@ -45,15 +80,11 @@ $(document).ready(function() {
 })
 
 
-
-
-
-
-
-function Asteroid(velocity, coordinates){
+function Asteroid(velocity, coordinates, radius){
 
   this.velocity = velocity
   this.coordinates = coordinates
+  this.radius = radius
   // this.tic = function(){
   //   this.coordinates[0] += this.velocity[0]
   //   this.coordinates[1] += this.velocity[1]
@@ -70,7 +101,7 @@ Asteroid.prototype.tic = function(){
 
 var asteroidSim = function(){
   asteroids = []
-  for (var i = 0; i < 10000; i++){
+  for (var i = 0; i < 10; i++){
     v1 = Math.floor(Math.random() * 100)
     v2 = Math.floor(Math.random() * 100)
     c1 = Math.floor(Math.random() * 100)
@@ -88,5 +119,10 @@ var asteroidSim = function(){
 
   console.log(Date.now() - start)
 
+}
+
+var sample = function(n){
+  vals = [n, -n]
+  return vals[Math.round(Math.random())]
 }
 

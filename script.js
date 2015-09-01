1a68d7868b26036b6f502bfc9e6468de93a68f0e
David <davidmeza@outlook.com>
2015-08-31 20:54:44 -0400

    finish spaceship movement

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ var asteroidsModel = {
 
   generateAsteroid: function (x, y, radius) {
 
-    vel1 = this.sample(10)
-    var v1 = Math.floor(Math.random() * vel1 + (vel1 / 10))
-    vel2 = this.sample(10)
-    var v2 = Math.floor(Math.random() * vel2 + (vel2 / 10))
+    vel1 = this.sample(2)
+    var v1 = Math.floor(Math.random() * vel1 + Math.ceil(vel1 / 10))
+    vel2 = this.sample(2)
+    var v2 = Math.floor(Math.random() * vel2 + Math.ceil(vel2 / 10))
 
     var c1 = x || Math.floor(Math.random() * view.canvasWidth)
     var c2 = y || Math.floor(Math.random() * view.canvasHeight)
@@ -33,8 +33,7 @@ var asteroidsModel = {
 
   updateAsteroids: function(){
     for (var i = 0; i < this.asteroids.length; i++){
-      this.asteroids[i].coordinates[0] += this.asteroids[i].velocity[0];
-      this.asteroids[i].coordinates[1] += this.asteroids[i].velocity[1];
+      this.asteroids[i].tic();
     }
     this.checkCollision();
   },
@@ -82,7 +81,12 @@ var view = {
 
   init: function () {
     this.setVars();
+    this.setListeners();
     this.render();
+  },
+
+  setListeners: function() {
+    $(document).on("keydown", this.moveSpaceship);
   },
 
   setVars: function() {
@@ -93,30 +97,47 @@ var view = {
   render: function(){
     setInterval(function(){
       asteroidsModel.updateAsteroids();
+      player.tic();
       view.draw();
-    }, 100);
+    }, 33);
   },
 
-  // drawSpaceShip: function(){
-  // ctx.beginPath();
-  // ctx.moveTo(28.4, 16.9);
-  // ctx.bezierCurveTo(28.4, 19.7, 22.9, 22.0, 16.0, 22.0);
-  // ctx.bezierCurveTo(9.1, 22.0, 3.6, 19.7, 3.6, 16.9);
-  // ctx.bezierCurveTo(3.6, 14.1, 9.1, 11.8, 16.0, 11.8);
-  // ctx.bezierCurveTo(22.9, 11.8, 28.4, 14.1, 28.4, 16.9);
-  // ctx.closePath();
-  // ctx.fillStyle = "rgb(0, 0, 255)";
-  // ctx.fill();
-  // ctx.beginPath();
-  // ctx.moveTo(22.3, 12.0);
-  // ctx.bezierCurveTo(22.3, 13.3, 19.4, 14.3, 15.9, 14.3);
-  // ctx.bezierCurveTo(12.4, 14.3, 9.6, 13.3, 9.6, 12.0);
-  // ctx.bezierCurveTo(9.6, 10.8, 12.4, 9.7, 15.9, 9.7);
-  // ctx.bezierCurveTo(19.4, 9.7, 22.3, 10.8, 22.3, 12.0);
-  // ctx.closePath();
-  // ctx.fillStyle = "rgb(255, 0, 0)";
-  // ctx.fill();
-  // }
+  moveSpaceship: function(event) {
+    switch (event.charCode || event.keyCode){ //
+        // turning
+        case 37: player.angle -= (Math.PI/3)/4;  break;
+        case 39: player.angle += (Math.PI/3)/4;  break;
+        // accelerator
+        case 38:
+          player.accelerate();
+          break;
+        // brake
+        case 40:
+          player.decelerate();
+          break;
+      }
+    },
+
+  drawUFO: function(){
+  ctx.beginPath();
+  ctx.moveTo(28.4, 16.9);
+  ctx.bezierCurveTo(28.4, 19.7, 22.9, 22.0, 16.0, 22.0);
+  ctx.bezierCurveTo(9.1, 22.0, 3.6, 19.7, 3.6, 16.9);
+  ctx.bezierCurveTo(3.6, 14.1, 9.1, 11.8, 16.0, 11.8);
+  ctx.bezierCurveTo(22.9, 11.8, 28.4, 14.1, 28.4, 16.9);
+  ctx.closePath();
+  ctx.fillStyle = "rgb(0, 0, 255)";
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(22.3, 12.0);
+  ctx.bezierCurveTo(22.3, 13.3, 19.4, 14.3, 15.9, 14.3);
+  ctx.bezierCurveTo(12.4, 14.3, 9.6, 13.3, 9.6, 12.0);
+  ctx.bezierCurveTo(9.6, 10.8, 12.4, 9.7, 15.9, 9.7);
+  ctx.bezierCurveTo(19.4, 9.7, 22.3, 10.8, 22.3, 12.0);
+  ctx.closePath();
+  ctx.fillStyle = "rgb(255, 0, 0)";
+  ctx.fill();
+  },
 
   drawSpaceShip: function(){
     this.context.save();
@@ -124,14 +145,14 @@ var view = {
     this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.context.restore();
     this.context.save();
-    this.context.translate(spaceShip.x,spaceShip.y);
-    this.context.rotate(angle);
+    this.context.translate(player.x, player.y);
+    this.context.rotate(player.angle);
     this.context.fillStyle = "rgba(150, 255, 0, 0.3)";
     this.context.strokeStyle = "#96FF00";
     this.context.beginPath();
-    this.context.moveTo(v[0][0],v[0][1]);
-    this.context.lineTo(v[1][0],v[1][1]);
-    this.context.lineTo(v[2][0],v[2][1]);
+    this.context.moveTo(player.lineLengths[0][0], player.lineLengths[0][1]);
+    this.context.lineTo(player.lineLengths[1][0], player.lineLengths[1][1]);
+    this.context.lineTo(player.lineLengths[2][0], player.lineLengths[2][1]);
     this.context.closePath();
     this.context.stroke();
     this.context.fill();
@@ -151,7 +172,7 @@ var view = {
   draw: function(){
     var canvas = this.canvas;
     var context = this.context
-    context.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+    canvas.height = canvas.height;
     this.drawSpaceShip();
     this.drawAsteroids();
 
@@ -164,18 +185,6 @@ var controller = {
     asteroidsModel.init();
     view.init();
   }
-}
-
-var spaceShip = {
-
-  x: view.canvasWidth / 2,
-  y: view.canvasHeight / 2,
-  lineLengths: [[0, -25], [-25, 0], [25, 0]]
-  vx: 0,
-  vy: 0,
-  angle: 0
-
-
 }
 
 $(document).ready(function() {
@@ -198,18 +207,60 @@ Asteroid.prototype.tic = function(){
 
 }
 
-// Two asteroids collide
-// For each of them, we divide the radius by 2
-// If radius is below 5 we remove it from screen
-// Otherwise we generate two more asteroids with the close coordinates but opposite directions
+function SpaceShip() {
 
-
-Asteroid.prototype.tic = function(){
-
-  this.coordinates[0] += this.velocity[0]
-  this.coordinates[1] += this.velocity[1]
+  this.x = view.canvasWidth / 2;
+  this.y = view.canvasHeight / 2;
+  this.lineLengths = [[0, -25], [-10, 0], [10, 0]];
+  this.velx = 0;
+  this.vely = 0;
+  this.angle = 0;
 
 }
+
+SpaceShip.prototype.tic = function() {
+  this.x += this.velx;
+  this.y += this.vely;
+  if (this.x < 0) {
+    this.x += view.canvasWidth;
+  } else if (this.x > view.canvasWidth) {
+    this.x -= view.canvasWidth
+  };
+
+  if (this.y < 0) {
+    this.y += view.canvasHeight;
+  } else if (this.y > view.canvasHeight) {
+    this.y -= view.canvasHeight
+  };
+}
+
+SpaceShip.prototype.accelerate = function() {
+  player.velx += Math.sin(player.angle);
+  player.vely += Math.cos(player.angle) * -1;
+
+  if (player.velx > 5 ) player.velx = 5;
+  if (player.velx < -5 ) player.velx = -5;
+  if (player.vely > 5) player.vely = 5;
+  if (player.vely < -5 ) player.vely = -5;
+  console.log("speed x: " + player.velx)
+  console.log("speed y: " + player.vely)
+}
+
+SpaceShip.prototype.decelerate = function() {
+  player.velx -= Math.sin(player.angle);
+  player.vely -= Math.cos(player.angle) * -1;
+
+  if (player.velx > 1 ) player.velx = 1;
+  if (player.velx < -1) player.velx = -1;
+  if (player.vely > 1) player.vely = 1;
+  if (player.vely < -1) player.vely = -1;
+  console.log("decelerating x: " + player.velx)
+  console.log("decelerating y: " + player.vely)
+}
+
+var player = new SpaceShip();
+
+
 
 function sortNumber(a,b) {
    return a - b;

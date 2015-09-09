d3de17dc0337a28f45d6b1abb6bc4b0e711c041c
Adam Kinson <adam.kinson@gmail.com>
2015-09-09 17:18:05 -0400

    add canvas with circle

--- asteroids.js
+++ asteroids.js
@@ -1,7 +1,27 @@
-var ASTEROIDS = {
+//var ASTEROIDS = ASTEROIDS || {};
 
-  var AsteroidConstructor = function(x,y,velX,velY) {
+var model = {
 
+
+}
+
+
+
+
+var view = {
+
+  drawAsteroid: function() {
+    var canvas = $('#playarea')[0].getContext('2d');
+    canvas.beginPath();
+    canvas.arc(200, 200, 25, 0, Math.PI * 2, false);
+    canvas.closePath();
+    canvas.strokeStyle = "#000";
+    canvas.stroke();
+  },
+
+
+  function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 }

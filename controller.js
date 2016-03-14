9d41ad8c127ee8672f50852a0f75f67faefaccdd
Julia Herron <julia.herron@gmail.com>
2016-03-13 21:30:22 -0700

    basics

---
+++ controller.js
@@ -0,0 +1,25 @@
+var GAME = GAME || {};
+
+// GAME.view.init()
+// GAME.model.generateAsteroids(10)
+// GAME.view.draw()
+
+
+
+GAME.controller = {
+
+  init: function() {
+    GAME.model.init();    
+  },
+
+
+  allAsteroids: GAME.model.asteroids,
+
+
+
+
+
+
+
+
+}

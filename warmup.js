909fc064290c0db32ebfacf5cd400529fc218401
Thomas Lo <thomasjinlo@gmail.com>
2016-03-14 11:33:07 -0700

    Finished warmpup

--- warmup.js
+++ warmup.js
@@ -1,26 +1,21 @@
-
-var gameModel ={
-
-}
-
-
 var model = {
 
-  collection:[],
+  collection: [],
 
-  Asteroid: function(X, Y, Xvel, Yvel){
-    this.X;
-    this.Y;
-    this.Xvel;
-    this.Yvel;
+  Asteroid: function(){
+    this.X = Math.floor(Math.random() * 1000);
+    this.Y = Math.floor(Math.random() * 1000);
+    this.Xvel = Math.floor(Math.random() * 10);
+    this.Yvel = Math.floor(Math.random() * 10);
+    return this
   },
 
   createAsteroid: function(){
     var asteroid = new this.Asteroid();
-    asteroid.X = 0;
-    asteroid.Y = 0;
-    asteroid.Xvel = 1;
-    asteroid.Yvel = 1;
+    asteroid.X = Math.floor(Math.random() * 1000);
+    asteroid.Y = Math.floor(Math.random() * 1000);
+    asteroid.Xvel = Math.floor(Math.random() * 10);
+    asteroid.Yvel = Math.floor(Math.random() * 10);
 
     this.collection.push(asteroid);
     return asteroid;
@@ -28,8 +23,21 @@ var model = {
 
   generateAsteroids: function(num){
     for(var i = 0; i < num; i++){
-      this.createAsteroid();
+      this.collection.push(new this.Asteroid());
     }
+  },
+
+  Asteroid2: function(){
+    this.X = Math.floor(Math.random() * 1000);
+    this.Y = Math.floor(Math.random() * 1000);
+    this.Xvel = Math.floor(Math.random() * 10);
+    this.Yvel = Math.floor(Math.random() * 10);
+
+    this.tic = function(){
+      this.X += this.Xvel;
+      this.Y += this.Yvel;
+    }
+    return this;
   }
 
 }
@@ -40,8 +48,47 @@ model.Asteroid.prototype.tic = function(){
   this.Y += this.Yvel;
 }
 
+
+// model.generateAsteroids(6);
+
 //this will be in the init--want your setInterval to act on all the key objects
 //in your game
 //setInterval(function(){asteroid.tic()}, 1000);
 
-model.generateAsteroids();
+// var update = setInterval(function(){
+//   model.collection.forEach(function(element){
+//     element.tic();
+//   } )
+// }, 1000)
+
+
+function testPerformance() {
+  var cstartDate = new Date();
+  for (var i = 0; i < 10000000; i++){
+    var ast = new model.Asteroid();
+    for (var i = 0; i < 10000000; i++){
+      ast.tic();
+    }
+  }
+  var cendDate = new Date();
+
+
+
+  var startDate = new Date();
+  for (var i = 0; i < 10000000; i++){
+    var ast2 = new model.Asteroid2();
+    for (var i = 0; i < 10000000; i++){
+      ast2.tic();
+    }
+  }
+  var endDate = new Date();
+
+
+var prototypeTime = cendDate.getTime() - cstartDate.getTime();
+var constructorTime = endDate.getTime() - startDate.getTime();
+
+alert("Constructor time: " + constructorTime + ", prototype time: " + prototypeTime);
+
+}
+
+testPerformance();

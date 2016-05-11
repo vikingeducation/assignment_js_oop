9d7501eef9e3ebb3ce30d0c76ca0c8e295598d76
Sia <siakaramalegos@gmail.com>
2016-05-11 18:00:05 -0500

    Adds tic method to Asteroid model

--- scripts/AsteroidModel.js
+++ scripts/AsteroidModel.js
@@ -1,5 +1,23 @@
 'use strict;'
 
 function AsteroidModel(){
-
+  this.xLocation = 0;
+  this.yLocation = 0;
+  this.xVelocity = 1;
+  this.yVelocity = 1;
 }
+
+// Asteroid methods
+AsteroidModel.prototype.tic = function(){
+  console.log('Moving from ' + this.xLocation + ', ' + this.yLocation);
+  this.xLocation += this.xVelocity;
+  this.yLocation += this.yVelocity;
+  console.log('...to ' + this.xLocation + ', ' + this.yLocation);
+};
+
+// Asteroid subclass
+function Asteroid(){}
+
+// Asteroid.prototype = Object.create(AsteroidModel.prototype);
+
+// instantiate in the controller?

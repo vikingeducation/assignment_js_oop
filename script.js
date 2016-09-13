0cf8b908f120177d1df27fed3ef40e02b02910ed
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-13 14:30:09 -0400

    Make asteroids

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-"use strict";
-//alert("Total time was: " + totalTime + " milliseconds");
-// function Asteroid (x, y) {
-//   this.x = x;
-//   this.y = y;
-//   this.xVelocity = 1;
-//   this.yVelocity = 1;
-// }
-
 function Asteroid (x, y) {
-  this.x = x;
-  this.y = y;
+  this.xPos = x;
+  this.yPos = y;
   this.xVelocity = 1;
   this.yVelocity = 1;
   this.tic = function () {
@@ -18,39 +9,42 @@ function Asteroid (x, y) {
   }
 }
 
-//Asteroid.prototype.tic = function() {
-  //this.x += this.xVelocity;
-  //this.y += this.yVelocity;
-//}
-
-var asteroid = new Asteroid(5, 5);
-asteroid.tic();
-console.log(asteroid.x);
-console.log(asteroid.y);
-
 var AsteroidField = {}
-
 
 for(var i = 0; i < 1000; i++) {
   var xPos = Math.floor(Math.random() * 100);
   var yPos = Math.floor(Math.random() * 100);
   AsteroidField["asteroid" + i] = new Asteroid(xPos, yPos);
   var asteroid = AsteroidField["asteroid" + i];
-  console.log("Asteroid number " + i + " was at location [" + asteroid.x + "," + asteroid.y + "]");
 }
 
-var firstTime = new Date();
+var canvas = document.getElementById("canvas");
+console.log(canvas);
+var ctx = canvas.getContext("2d");
 
-for(var i = 0; i < 1000; i++) {
-  var asteroid = AsteroidField["asteroid" + i];
-  for(var j = 0; j < 1000; j++) {
-    asteroid.tic();
+
+// ctx.beginPath();
+// ctx.arc(xpos, ypos, 5, 0, 2 * Math.PI);
+// ctx.stroke();
+
+var a = new Asteroid(75, 75);
+
+var view = {
+  init: function() {
+
+  },
+
+  render: function() {
+    setInterval(view.moveAsteroid(a), 100);
+  },
+
+  moveAsteroid: function(a) {
+    console.log("dsads")
+    a.tic();
+    ctx.beginPath();
+    ctx.arc(a.xPos, a.yPos, 5, 0, 2 * Math.PI);
+    ctx.stroke();
   }
 }
 
-var secondTime = new Date();
-
-var totalTime =  secondTime.getTime() - firstTime.getTime()
-
-
-
+view.render();

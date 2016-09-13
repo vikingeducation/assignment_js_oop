dcb21d379023c83ed5565a07d3a85b9909407e0c
Hannah Squier <hannahsquier@gmail.com>
2016-09-13 10:18:43 -0700

    set up asteroid creation

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
-function Asteroid(xPos, yPos, xVel, yVel) {
-  this.xPos = xPos;
-  this.yPos = yPos;
-  this.xVel = xVel;
-  this.yVel = yVel;
-}
+CANVAS_WIDTH = 800;
+CANVAS_HEIGHT = 600;
 
 Asteroid.prototype.tic = function() {
   this.xPos += this.xVel;
   this.yPos += this.yVel;
 }
 
-function AsteroidConst(xPos, yPos, xVel, yVel) {
-  this.xPos = xPos;
-  this.yPos = yPos;
-  this.xVel = xVel;
-  this.yVel = yVel;
-  this.tic = function() {
-    this.xPos += this.xVel;
-    this.yPos += this.yVel;
-  };
-}
+
 
 // a = new Asteroid(0, 0, 1, 1);
 // a.tic
+model = {
+
+  init: function() {
+    this.asteroids = buildAsteroids();
+  },
+
+  buildAsteroids: function() {
+
+    var asteroids = []
+    for (var i = 0; i < 1000; i++) {
+      asteroids.push(createAsteroid());
+    }
+    return asteroids
+  },
+
+  createAsteroid: function() {
+    var enterEdge = Math.floor(Math.random() * 4)
+
+      var xPos, yPos, xVel, yVel;
+      // enter top
+      if(enterEdge === 0) {
+        xPos = Math.floor(Math.random() * CANVAS_WIDTH)
+        yPos = 0
+        xVel = Math.floor(Math.random() * 5)
+        yVel = Math.floor(Math.random() * 5)
+      } else if(enterEdge === 1) { // enter right
+        xPos = CANVAS_WIDTH
+        yPos = Math.floor(Math.random() * CANVAS_HEIGHT)
+        xVel = Math.floor(Math.random() * 5) * -1
+        yVel = Math.floor(Math.random() * 5)
+      } else if(enterEdge === 2) { // enter bottom
+        xPos = Math.floor(Math.random() * CANVAS_WIDTH)
+        yPos = CANVAS_HEIGHT
+        xVel = Math.floor(Math.random() * 5)
+        yVel = Math.floor(Math.random() * 5)  * -1
+      } else { // enter left
+        xPos = 0
+        yPos = Math.floor(Math.random() * CANVAS_HEIGHT)
+        xVel = Math.floor(Math.random() * 5)
+        yVel = Math.floor(Math.random() * 5)
+      }
+
+      return new Asteroid(xPos,yPos,xVel,yVel);
+
+  },
+
+  Asteroid: function(xPos, yPos, xVel, yVel) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.xVel = xVel;
+    this.yVel = yVel;
+  }
+}
 
 controller = {
   // play: function() {
@@ -31,55 +71,12 @@ controller = {
   //   }, 1000);
   // }
 
-  benchmark: function() {
-    var astArray = []
-    for (var i = 0; i < 1000; i++) {
-      var xPos = Math.floor(Math.random() * 100)
-      var yPos = Math.floor(Math.random() * 100)
-      var xVel = Math.floor(Math.random() * 10)
-      var yVel = Math.floor(Math.random() * 10)
-      var newAst = new Asteroid(xPos,yPos,xVel,yVel);
-      astArray.push(newAst);
-    }
 
-    var timeBefore = new Date();
+}
 
-    for (var i = 0; i < astArray.length; i++) {
-      for (var j = 0; j < 1000; j++) {
-        astArray[i].tic();
-      }
-    }
+view = {
+  render: function(asteroids) {
 
-    var timeAfter = new Date();
-
-    var timeElapsedProto = timeAfter.getTime() - timeBefore.getTime();
-
-    astArray = []
-    for (var i = 0; i < 1000; i++) {
-      var xPos = Math.floor(Math.random() * 100)
-      var yPos = Math.floor(Math.random() * 100)
-      var xVel = Math.floor(Math.random() * 10)
-      var yVel = Math.floor(Math.random() * 10)
-      var newAst = new AsteroidConst(xPos,yPos,xVel,yVel);
-      astArray.push(newAst);
-    }
-
-    timeBefore = new Date();
-
-    for (var i = 0; i < astArray.length; i++) {
-      for (var j = 0; j < 1000; j++) {
-        astArray[i].tic();
-      }
-    }
-
-    timeAfter = new Date();
-
-    var timeElapsedInher = timeAfter.getTime() - timeBefore.getTime();
-
-
-
-    console.log("Tic time for Proto: " + timeElapsedProto);
-    console.log("Tic time for Inher: " + timeElapsedInher);
   }
 }
 

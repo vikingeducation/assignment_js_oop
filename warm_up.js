c964eb6dbf836c1b0c4dac302cae3e9774a41530
Adrian Mui <adrian.mui@gmail.com>
2016-09-13 11:50:17 -0500

    warmup fin

--- warm_up.js
+++ warm_up.js
@@ -12,13 +12,42 @@ APP.Asteroid.prototype.tic = function() {
   this.coordY += this.velocityY;
 };
 
-
 var asteroid = new APP.Asteroid(2,2,1,1);
 
-APP.buildManyAsteroids = function (numOfAsteroids) {
+APP.buildManyAsteroids = function (numOfAsteroids, numOfTics) {
+  var staticObjectStartDateTime = new Date();
   var asteroids = [];
   for (var i=0; i < numOfAsteroids; i++) {
-    var randomX = Math.floor((Math.random() * (5 - 1) + 1));
-    var randomY = Math.
+    var randX = Math.floor(Math.random() * 10) + 1;
+    var randY = Math.floor(Math.random() * 10) + 1;
+    var randVelocityX = Math.floor(Math.random() * 3) + 1;
+    var randVelocityY = Math.floor(Math.random() * 3) + 1;
+
+    var newAsteroid = new APP.Asteroid(randX, randY, randVelocityX, randVelocityY);
+
+    newAsteroid.tic = function() {
+      this.coordX += this.velocityX;
+      this.coordY += this.velocityY;
+    };
+
+    asteroids.push(newAsteroid);
   }
+
+  //do this tic times
+  for (var i=0; i < numOfTics; i++) {
+    asteroids.forEach(function(el) { 
+      el.tic();
+    });
+  }
+
+  var staticObjectEndDateTime = new Date();
+
+  var staticTime = staticObjectEndDateTime.getTime() - staticObjectStartDateTime.getTime();
+
+  console.log(staticTime + " milliseconds");
 };
+
+// using chrome
+//prototype : 18 ms
+//constructor: 30 ms
+APP.buildManyAsteroids(1000, 1000);

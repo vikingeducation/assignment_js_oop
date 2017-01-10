fb01e2dbb0aecae7a6eda7bc5da4f6c9ee577401
James Harris <computersherpa@gmail.com>
2017-01-10 15:55:47 -0600

    render ship

--- js/view.js
+++ js/view.js
@@ -34,16 +34,27 @@ view.moveAsteroid = function(viewAsteroid) {
 
 view.createShip = function(modelShip) {
   var viewShip = view.two.makeCircle(modelShip.coords.x, modelShip.coords.y, modelShip.radius);
+  viewShip.fill = 'magenta';
+
   var muzzle = view.two.makeRectangle(modelShip.coords.x,
     modelShip.coords.y - modelShip.radius,
-    10, 3)
-  viewShip.fill = 'magenta';
+    10, 3);
   muzzle.fill = '#4285F4';
+
+  var leftFin = view.two.makePolygon(modelShip.coords.x - modelShip.radius + 3, modelShip.coords.y + modelShip.radius - 2, 5);
+  leftFin.rotation = -Math.PI/8;
+  leftFin.fill = 'magenta';
+
+  var rightFin = view.two.makePolygon(modelShip.coords.x + modelShip.radius - 3, modelShip.coords.y + modelShip.radius - 2, 5);
+  rightFin.rotation = Math.PI/8;
+  rightFin.fill = 'magenta';
 
   var shipGroup = view.two.makeGroup();
   shipGroup.add(viewShip);
   shipGroup.add(muzzle);
-}
+  shipGroup.add(leftFin);
+  shipGroup.add(rightFin);
+};
 
 
 // var circle = view.two.makeCircle(72, 100, 50);

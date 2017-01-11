506126c1ea362a8d29443a2c3cf0e2a99949f696
Catherine Kwak <khopsickle@users.noreply.github.com>
2017-01-10 17:00:18 -0700

    The ship rotates!

--- game_window.js
+++ game_window.js
@@ -11,6 +11,7 @@ APP.gameWindow = {
   init: function() {
     this.canvases.push(APP.shipCanvas);
     this.canvases.push(APP.asteroidCanvas);
+    $(document).keydown(this._controlShip);
   },
 
   updateContexts: function() {
@@ -21,7 +22,23 @@ APP.gameWindow = {
   clearContexts: function() {
     this.canvases.forEach(function(canvas) {
       canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    })
+    });
+  },
+
+  _controlShip: function(e) {
+    var keys = [37, 38, 39],
+        direction;
+    if (e.which === 37) {
+      direction = 'l';
+    } else if (e.which === 38) {
+      this.ship.boost();
+    } else if (e.which === 39) {
+      direction = 'r';
+    }
+
+    if (direction) {
+      APP.gameWindow.ship.rotate(direction);
+    }
   },
 
   _moveShip: function() {
--- ship.js
+++ ship.js
@@ -10,7 +10,24 @@ APP.ship = function Ship(options) {
   };
   this.height = APP.CONFIG.ship.height;
   this.width = APP.CONFIG.ship.width;
+  this.deg = 5;
   APP.shape.call(this, options);
+  // this.angle =
+  // this.boost = function() {
+  //
+  // };
+  this.rotate = function(direction) {
+    // clear existing triangle
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    // translate the pivot point to the center of the triangle
+    this.ctx.translate(this.coords.x, this.coords.y);
+    // rotate the triangle by 5 deg
+    this.ctx.rotate(this.deg * Math.PI / 180);
+    // translate pivot point back
+    this.ctx.translate(-this.coords.x, -this.coords.y);
+    // draw the new triangle
+    this.draw();
+  };
   APP.gameWindow.ship = this;
 };
 
@@ -23,10 +40,12 @@ APP.ship.init = function() {
 };
 
 APP.ship.prototype.draw = function() {
+
   this.ctx.beginPath(); // center
   this.ctx.moveTo(this.coords.x, this.coords.y + this.height * (2 / 3)); // first point
   this.ctx.lineTo(this.coords.x + this.width / 2, this.coords.y - this.height / 3); // 2nd point
   this.ctx.lineTo(this.coords.x - this.width / 2,this.coords.y - this.height / 3); // 3rd point
+  // this.ctx.lineTo(this.coords.x - this.width / 2,this.coords.y - this.height / 3); // back to first point
   this.ctx.fill(); //
   this.ctx.closePath();
 };
